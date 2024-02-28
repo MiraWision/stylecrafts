04a221b7b74b3d3cf2102f6251c0a1dc8c9f4c39
Yelysei Lukin <yelysei277@gmail.com>
2024-02-28 20:22:55 +0100

    Basic setup

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css';
+import 'primereact/resources/themes/lara-dark-pink/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+interface Props {
+  Component: any;
+  pageProps: any;
+}
+
+const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
