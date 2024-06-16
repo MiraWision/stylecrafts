794f962203514002a146d6913c4ec84ac159219e
Yelysei Lukin <yelysei277@gmail.com>
2024-06-16 13:22:40 +0200

    Improve meta tags

--- components/pages/meta-tags.tsx
+++ components/pages/meta-tags.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 
 import { MetaTags as IMetaTags } from '@/content/meta-data/types';
 
-const defaultImage = '../../public/meta-data/image-1.jpeg';
+const defaultImage = '/meta-data/image-1.jpeg';
 
 const MetaTags: React.FC<IMetaTags> = ({ title, description, keywords, image, url }) => {
   const router = useRouter();
@@ -20,7 +20,7 @@ const MetaTags: React.FC<IMetaTags> = ({ title, description, keywords, image, ur
       <meta property='og:description' content={description} />
       <meta property='og:image' content={image || defaultImage} />
       <meta property='og:image:width' content='1200' />
-      <meta property='og:image:height' content='627' />
+      <meta property='og:image:height' content='630' />
       <meta property='og:url' content={`${url}${router.asPath}`} />
       <meta property='og:locale' content='en_US' />
       <meta property='og:type' content='website' />
@@ -28,7 +28,7 @@ const MetaTags: React.FC<IMetaTags> = ({ title, description, keywords, image, ur
       <meta name='twitter:description' content={description} />
       <meta name='twitter:image' content={image || defaultImage} />
       <meta name='twitter:image:width' content='1200' />
-      <meta name='twitter:image:height' content='627' />
+      <meta name='twitter:image:height' content='630' />
       <meta name='twitter:card' content='summary_large_image' />
     </Head>
   );
